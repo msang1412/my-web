65e7bc31a8abce89c6bab20718581dc45c4c44c8
msang1412 <yiweiho881@gmail.com>
2025-12-12 14:24:33 +0700

    Update counter.php

--- counter.php
+++ counter.php
@@ -4,17 +4,26 @@ header('Access-Control-Allow-Origin: *');
 
 $counter_file = 'counter.txt';
 
+// Đọc số lượt
 if (file_exists($counter_file)) {
     $count = (int)file_get_contents($counter_file);
 } else {
     $count = 0;
 }
 
-$count++; // Tăng lên 1 mỗi lần script gọi
+// Tăng counter nếu có ?increase=1
+if (isset($_GET['increase'])) {
+    $count++;
+    file_put_contents($counter_file, $count);
+}
 
-file_put_contents($counter_file, $count);
-
+// Xuất JSON giống Lanyard style
 echo json_encode([
-    "status" => true,
-    "total_executions" => $count
-]);
+    "success" => true,
+    "data" => [
+        "script_name" => "Your Script",
+        "total_executions" => $count,
+        "author" => "Minh Sang",
+        "github" => "https://github.com",
+    ]
+], JSON_PRETTY_PRINT);
