ec82f5662fad50cc059f2289420330c7db5a1e37
msang1412 <yiweiho881@gmail.com>
2026-01-05 16:58:28 +0700

    Create gpo.lua

---
+++ gpo.lua
@@ -0,0 +1,10 @@
+getgenv().Config = {
+  ['Private Server'] = true,
+  ['Lock Level'] = 456,
+  ['Lock Stat'] = 700,
+  ['Multi PS'] = true,
+  ['Private Code'] = {""}
+}
+getgenv().Sea = 1
+script_key = "xQcVJKrfvzOGZhzEccXzQdrIuOqXpESM";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0c77fdc97b797339625442ae88021b1e.lua"))()
