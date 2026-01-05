7f6441db615a9e8a1f5da3f5d75c982abb425ec0
msang1412 <yiweiho881@gmail.com>
2026-01-05 16:59:08 +0700

    Create mcgpo.lua

---
+++ mcgpo.lua
@@ -0,0 +1,9 @@
+getgenv().PrivateCode = "c8PxZny9Hp"
+getgenv().TradeHub = false
+getgenv().Sea = 1
+script_key="xQcVJKrfvzOGZhzEccXzQdrIuOqXpESM";
+pcall(function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/QuynhLaSo1/refs/heads/main/radiant.lua"))()
+
+end)
