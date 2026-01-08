223eff3a93896d2b024ccf6d2eff9e69e165a8cb
msang1412 <yiweiho881@gmail.com>
2026-01-08 19:56:21 +0700

    Update yeuem.lua

--- yeuem.lua
+++ yeuem.lua
@@ -39,5 +39,5 @@ if ps[player.Name] then
 end
 
 getgenv().Sea = 1
-script_key = "xQcVJKrfvzOGZhzEccXzQdrIuOqXpESM";
+script_key = "yjXgqkaIFaSWfjDqPkOWiZdRMPYajdiK";
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0c77fdc97b797339625442ae88021b1e.lua"))()
