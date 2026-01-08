9b8eac013c20234f5756639b6dc1da2da92ec8f2
msang1412 <yiweiho881@gmail.com>
2026-01-08 12:04:04 +0700

    Update handless.lua

--- handless.lua
+++ handless.lua
@@ -1,9 +1,7 @@
 getgenv().PrivateCode = "0PRW0jL24u"
 getgenv().TradeHub = false
 getgenv().Sea = 1
-script_key="xQcVJKrfvzOGZhzEccXzQdrIuOqXpESM";
+script_key="yjXgqkaIFaSWfjDqPkOWiZdRMPYajdiK";
 pcall(function()
-
 loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/QuynhLaSo1/refs/heads/main/radiant.lua"))()
-
 end)
