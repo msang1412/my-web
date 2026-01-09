b389a8f93b03ba11dc598549850577da721cb40a
msang1412 <yiweiho881@gmail.com>
2026-01-09 09:57:00 +0700

    Update handless.lua

--- handless.lua
+++ handless.lua
@@ -1,1 +1,1 @@
-game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("reserved"):InvokeServer("tV0pYMpaUr")
+game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("reserved"):InvokeServer("0PRW0jL24u")
