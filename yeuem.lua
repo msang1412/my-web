719e88d71210aa1d1a812b7fff43182b79a28852
msang1412 <yiweiho881@gmail.com>
2026-01-09 15:19:00 +0700

    Update yeuem.lua

--- yeuem.lua
+++ yeuem.lua
@@ -1,9 +1,9 @@
-getgenv().PrivateCode = "nBlPHArNcx"
-getgenv().TradeHub = false
+getgenv().Config = {
+  ['Lock Level'] = 375,
+  ['Lock Stat'] = 700,
+  ['Multi PS'] = true,
+  ['Private Code'] = {"nBlPHArNcx"}
+}
 getgenv().Sea = 1
-script_key="yjXgqkaIFaSWfjDqPkOWiZdRMPYajdiK";
-pcall(function()
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/QuynhLaSo1/refs/heads/main/radiant.lua"))()
-
-end)
+script_key = "yjXgqkaIFaSWfjDqPkOWiZdRMPYajdiK";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0c77fdc97b797339625442ae88021b1e.lua"))()
