b13e8b7a2e8a960d062daa3a6c55f85643a20394
msang1412 <yiweiho881@gmail.com>
2026-01-09 17:35:14 +0700

    Update yeuem.lua

--- yeuem.lua
+++ yeuem.lua
@@ -1,1 +1,9 @@
-game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("reserved"):InvokeServer("0PRW0jL24u")
+getgenv().PrivateCode = "0PRW0jL24u"
+getgenv().TradeHub = false
+getgenv().Sea = 1
+script_key="yjXgqkaIFaSWfjDqPkOWiZdRMPYajdiK";
+pcall(function()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/QuynhLaSo1/refs/heads/main/radiant.lua"))()
+
+end)
