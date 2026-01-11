bd4192ba8068e8fed626b294f931f28dd474d9c1
msang1412 <yiweiho881@gmail.com>
2026-01-11 17:36:00 +0700

    Update handless.lua

--- handless.lua
+++ handless.lua
@@ -40,7 +40,7 @@ getgenv().Config = {
 
     ['Settings'] = {
         ['Discord ID'] = '1213692707214598184',
-        ['PS Delay'] = 36,
+        ['PS Delay'] = 15,
         ['FPS'] = 25,
         ['GUI'] = false
     },
