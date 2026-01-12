0f8736ff29109410d95106d604f14926aa222df5
msang1412 <yiweiho881@gmail.com>
2026-01-12 08:43:15 +0700

    Update handless.lua

--- handless.lua
+++ handless.lua
@@ -1,38 +1,79 @@
 local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
 local player = Players.LocalPlayer or Players.PlayerAdded:Wait()
 
--- Private Server theo username
 local ps = {
-    Ph03nix_M00N50     = "nBlPHArNcx",
-    XxHazePowerxX96   = "nBlPHArNcx",
+    Ph03nix_M00N50 = "nBlPHArNcx",
+    XxHazePowerxX96 = "nBlPHArNcx",
+
     TurboCrazeMystic2005 = "bGm6GQ6ZVb",
-    LaylaShadowQueen45     = "bGm6GQ6ZVb",
-    UltraMast3r201034   = "tV0pYMpaUr",
+    LaylaShadowQueen45 = "bGm6GQ6ZVb",
+
+    UltraMast3r201034 = "tV0pYMpaUr",
     Xx_EmmaCircuitSparkl = "tV0pYMpaUr",
-    HawkTurboBacon53     = "JUGHWiUw0T",
-    EzraKing202481   = "JUGHWiUw0T",
+
+    HawkTurboBacon53 = "JUGHWiUw0T",
+    EzraKing202481 = "JUGHWiUw0T",
+
     FlashClawN3on2020 = "BJG2x8SsVL",
-    JAM3S_Danc3r89     = "BJG2x8SsVL",
-    SamuelP0werC00kie200   = "Kg8iGai7fZ",
+    JAM3S_Danc3r89 = "BJG2x8SsVL",
+
+    SamuelP0werC00kie200 = "Kg8iGai7fZ",
     XxJaydenProPrimalxX = "Kg8iGai7fZ",
-    Fusi0n_Wraith201768     = "PiHn10meSk",
-    AidenSonicStormy2021   = "PiHn10meSk",
+
+    Fusi0n_Wraith201768 = "PiHn10meSk",
+    AidenSonicStormy2021 = "PiHn10meSk",
+
     XxProHeroCookiexXYT = "lX6jT9kJxh",
 }
 
--- Config
+repeat task.wait(0.5) until game:IsLoaded() and player
+
+getgenv().Key = "Radiant_814454014171021322"
+getgenv().Speed = 50
+
 getgenv().Config = {
-    ['Lock Level'] = 567,
-    ['Lock Stat'] = 700,
-    ['Multi PS'] = true,
-    ['Private Code'] = {}
+    ['Multi Acc'] = 17,
+    ['Kaitun Mythical Chest'] = true,
+    ['Acc Desync'] = false,
+
+    ['Settings'] = {
+        ['Discord ID'] = '1213692707214598184',
+        ['PS Delay'] = 36,
+        ['FPS'] = 30,
+        ['GUI'] = false
+    },
+
+    ['Merchant'] = {
+        ['Rare Fruit Chest'] = false,
+        ['Legendary Fruit Chest'] = false,
+    },
+
+    ['Private Server'] = true,
+    ['Private Code'] = '',
+    ['Tracker'] = false,
+
+    ['Webhook'] = 'https://discord.com/api/webhooks/1427165628778938408/C_KNvE0DpZH8utzct_ruiNHl6GfjhN1H4uMdmYS45k8YPrsDLKbw4XQrKnXKG3Xwgd86'
 }
 
--- GĂ¡n PS theo ngÆ°á»i chÆ¡i
-if ps[player.Name] then
-    table.insert(getgenv().Config['Private Code'], ps[player.Name])
+local username = player.Name
+if ps[username] then
+    getgenv().Config['Private Code'] = ps[username]
 end
 
-getgenv().Sea = 1
-script_key = "xQcVJKrfvzOGZhzEccXzQdrIuOqXpESM";
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/0c77fdc97b797339625442ae88021b1e.lua"))()
+task.delay(60, function()
+    if not getgenv().Loaded then
+        TeleportService:Teleport(game.PlaceId)
+    end
+end)
+
+repeat
+    task.wait(5)
+    local success = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/RuneGuard/refs/heads/main/pro.lua"))()
+    end)
+    if not success then
+        task.wait(15)
+    end
+until getgenv().Loaded
