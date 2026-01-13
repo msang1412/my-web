fb5fe5688e4dea467c0b449c496f45da1cf2b2cc
msang1412 <yiweiho881@gmail.com>
2026-01-13 08:04:12 +0700

    Update yeuem2.lua

--- yeuem2.lua
+++ yeuem2.lua
@@ -1,49 +1,29 @@
-repeat task.wait(0.5) until game:IsLoaded() and player
-
+repeat wait(0.5) until game:IsLoaded() and game.Players.LocalPlayer
 getgenv().Key = "Radiant_814454014171021322"
 getgenv().Speed = 50
-
 getgenv().Config = {
-    ['Multi Acc'] = 20,
+    ['Multi Acc'] = 5,
     ['Kaitun Mythical Chest'] = true,
-    ['Acc Desync'] = true,
-
+    ['Acc Desync'] = false,
     ['Settings'] = {
         ['Discord ID'] = '1213692707214598184',
         ['PS Delay'] = 36,
         ['FPS'] = 20,
         ['GUI'] = false
     },
-
     ['Merchant'] = {
         ['Rare Fruit Chest'] = false,
         ['Legendary Fruit Chest'] = false,
     },
-
     ['Private Server'] = true,
     ['Private Code'] = 'ezwFiTEd1U',
     ['Tracker'] = false,
-
-    ['Webhook'] = 'https://discord.com/api/webhooks/1427165628778938408/C_KNvE0DpZH8utzct_ruiNHl6GfjhN1H4uMdmYS45k8YPrsDLKbw4XQrKnXKG3Xwgd86'
+    ['Webhook'] = 'https://discord.com/api/webhooks/1317008318979506186/7cHRjfhewaO7_F7AlFpOHNbL5t1272e_VZ3aQv1AV7j9ya0ea-dbsGmhs86IZCpODptT'
 }
-
-local username = player.Name
-if ps[username] then
-    getgenv().Config['Private Code'] = ps[username]
-end
-
-task.delay(60, function()
-    if not getgenv().Loaded then
-        TeleportService:Teleport(game.PlaceId)
-    end
+task.delay(60, function() if not getgenv().Loaded then game:GetService("TeleportService"):Teleport(game.PlaceId) end end)
+repeat task.wait(5)
+    local success, err = pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/RuneGuard/refs/heads/main/pro.lua"))()
 end)
-
-repeat
-    task.wait(5)
-    local success = pcall(function()
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/RuneGuard/refs/heads/main/pro.lua"))()
-    end)
-    if not success then
-        task.wait(15)
-    end
+    if not success then task.wait(15) end
 until getgenv().Loaded
