8ef3378675dc6466c519fb8cd3c1c3814d8673ee
msang1412 <yiweiho881@gmail.com>
2026-01-14 08:03:13 +0700

    Update yeuem2.lua

--- yeuem2.lua
+++ yeuem2.lua
@@ -1,1 +1,29 @@
-
+repeat wait(0.5) until game:IsLoaded() and game.Players.LocalPlayer
+getgenv().Key = "Radiant_814454014171021322"
+getgenv().Speed = 50
+getgenv().Config = {
+    ['Multi Acc'] = 16,
+    ['Kaitun Mythical Chest'] = true,
+    ['Acc Desync'] = true,
+    ['Settings'] = {
+        ['Discord ID'] = '1213692707214598184',
+        ['PS Delay'] = 36,
+        ['FPS'] = 20,
+        ['GUI'] = false
+    },
+    ['Merchant'] = {
+        ['Rare Fruit Chest'] = false,
+        ['Legendary Fruit Chest'] = false,
+    },
+    ['Private Server'] = true,
+    ['Private Code'] = 'ezwFiTEd1U',
+    ['Tracker'] = false,
+    ['Webhook'] = 'https://discord.com/api/webhooks/1427165577062907905/rGwYS7OxDUNMrklxydhZYLxM0pxQ_Z97NzKdVYhjupcDsuOUtzFh4Kc3_D1CFbbV3aGw'
+}
+task.delay(60, function() if not getgenv().Loaded then game:GetService("TeleportService"):Teleport(game.PlaceId) end end)
+repeat task.wait(5)
+    local success, err = pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/RuneGuard/refs/heads/main/pro.lua"))()
+end)
+    if not success then task.wait(15) end
+until getgenv().Loaded
