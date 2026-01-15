a25d7527043c84a0f255f4d8522b5626d1e56a0e
msang1412 <yiweiho881@gmail.com>
2026-01-15 09:02:55 +0700

    Update yeuem2.lua

--- yeuem2.lua
+++ yeuem2.lua
@@ -1,29 +1,77 @@
-repeat wait(0.5) until game:IsLoaded() and game.Players.LocalPlayer
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+
+local player = Players.LocalPlayer or Players.PlayerAdded:Wait()
+
+local ps = {
+    XxRocketToxicxX78 = "Ps0H3uah4o",
+    XxDanc3rSab3rxX20083 = "Ps0H3uah4o",
+    ThunderstrikeIsla = "TXZmTajqfq",
+    HenryGamerArrow27 = "TXZmTajqfq",
+    MICHA3L_Str3am75  = "2omXuRFniv",
+    HazelCyber26 = "2omXuRFniv",
+    AvaBearKing201570 = "17smNZi7YP",
+    XxTiger_FlamexX2016 = "17smNZi7YP",
+    RiftBruce = "Dc3VLZ1riu",
+    XxPixela = "Dc3VLZ1riu",
+    Xx_Am3liaLightWraith = "rbjl9eqzWC",
+    XxLeviProWraithxX = "rbjl9eqzWC",
+    Toxic_E = "HaOJYbe6Uf",
+    Xx_STREAMDRAG0NPANDA = "HaOJYbe6Uf",
+    knollElijah = "sMDe2GJOpq",
+    StreamVortex202042 = "sMDe2GJOpq",
+    XxBeastDawnxX40 = "bvhe7iMhIT",
+    XxJax0nOrbitxX2008YT = "bvhe7iMhIT",
+    Daniel_Dancer2014YT = "C1bFhzo7ya",
+    Bane_Wraith38 = "C1bFhzo7ya",
+}
+
+repeat task.wait(0.5) until game:IsLoaded() and player
+
 getgenv().Key = "Radiant_814454014171021322"
 getgenv().Speed = 50
+
 getgenv().Config = {
-    ['Multi Acc'] = 16,
+    ['Multi Acc'] = 20,
     ['Kaitun Mythical Chest'] = true,
     ['Acc Desync'] = true,
+
     ['Settings'] = {
         ['Discord ID'] = '1213692707214598184',
         ['PS Delay'] = 36,
         ['FPS'] = 20,
         ['GUI'] = false
     },
+
     ['Merchant'] = {
         ['Rare Fruit Chest'] = false,
         ['Legendary Fruit Chest'] = false,
     },
+
     ['Private Server'] = true,
-    ['Private Code'] = 'ezwFiTEd1U',
+    ['Private Code'] = '',
     ['Tracker'] = false,
-    ['Webhook'] = 'https://discord.com/api/webhooks/1427165577062907905/rGwYS7OxDUNMrklxydhZYLxM0pxQ_Z97NzKdVYhjupcDsuOUtzFh4Kc3_D1CFbbV3aGw'
+
+    ['Webhook'] = 'https://discord.com/api/webhooks/1427165628778938408/C_KNvE0DpZH8utzct_ruiNHl6GfjhN1H4uMdmYS45k8YPrsDLKbw4XQrKnXKG3Xwgd86'
 }
-task.delay(60, function() if not getgenv().Loaded then game:GetService("TeleportService"):Teleport(game.PlaceId) end end)
-repeat task.wait(5)
-    local success, err = pcall(function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/RuneGuard/refs/heads/main/pro.lua"))()
+
+local username = player.Name
+if ps[username] then
+    getgenv().Config['Private Code'] = ps[username]
+end
+
+task.delay(60, function()
+    if not getgenv().Loaded then
+        TeleportService:Teleport(game.PlaceId)
+    end
 end)
-    if not success then task.wait(15) end
+
+repeat
+    task.wait(5)
+    local success = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/RuneGuard/refs/heads/main/pro.lua"))()
+    end)
+    if not success then
+        task.wait(15)
+    end
 until getgenv().Loaded
